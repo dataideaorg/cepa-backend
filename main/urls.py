378624c77abbf8b6b8567901caa69e01297fb588
jumashafara <jumashafara0@gmail.com>
2025-09-20 15:36:12 +0300

    fix: updated endpoint

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('resources.urls')),
+    path('resources/', include('resources.urls')),
 ]
 
 # Serve media files
