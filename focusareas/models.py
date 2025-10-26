b9ab32bf1ddcff4a61097b154e15bba05825976c
jumashafara <jumashafara0@gmail.com>
2025-10-26 19:36:33 +0300

    fix: made migrations

--- focusareas/models.py
+++ focusareas/models.py
@@ -76,6 +76,7 @@ class FocusAreaActivity(models.Model):
     id = models.CharField(max_length=255, primary_key=True, default=generate_uuid)
     focus_area = models.ForeignKey(FocusArea, on_delete=models.CASCADE, related_name='activities')
     text = models.TextField()
+    description = models.TextField(blank=True, null=True)
     order = models.IntegerField(default=0)
 
     class Meta:
